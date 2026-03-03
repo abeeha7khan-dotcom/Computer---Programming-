2109e6cb16dab5cfbf6dc4c42370cc2aad599a25
Abeeha <abeeha7khan@gmail.com>
2026-03-03 19:12:29 +0500

    Add files via upload

---
+++ Lab-Reports/lab 2 task 1.cpp
@@ -0,0 +1,97 @@
+LAB 02
+task 1
+program to run
+nothing will print
+#include<iostream>
+using namespace std;
+int main(){
+ return 0;
+}
+
+
+
+task 2: print hello world
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Hello World!!"<<endl;
+	 cout<<"Abeeha"<<endl;
+	 cout<<"BF25NWELE0663"<<endl;
+	 cout<<"2007"<<endl;
+     cout<<"3"<<endl;	 
+	cout<<"bumblebee"<<endl; 
+	cout<<"Dear customer the service will be not available today. Sorry for inconvenience created"<<endl; 
+	cout<<"Happy to help!..."<<endl;
+	cout<<"Great question! ..."<<endl;
+	cout<<"Nice to meet you! ..."<<endl;
+	cout<<"May I ask why that is? ..."<<endl;
+	cout<<"Thanks for bringing this to our attention! ..."<<endl;
+	cout<<"I completely understand why you'd want that."<<endl;
+
+	return 0;
+}
+
+
+
+Task 3:Initialize a variable by running the following code 
+#include<iostream>
+using namespace std;
+int main(){
+float a;   //replace int by float then try integer and fraction
+//    a = 10; //change 10 by another integer and then change it to fraction number
+   a = 9.8;  // if we put it in fraction we get only 9 not 9.8
+	cout<<a;
+	return 0;
+}
+
+
+
+Task 4: Prompt to user and use of cin. 
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"kindly enter your name:";
+	cin>>a;
+	cout<<a; //write this after running the first program.
+	return 0;
+}
+ 
+
+
+//Task 5: Sequential: Print your name 10 times
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"abeeha"<<endl;
+	cout<<"pakistan"<<endl;
+	cout<<"pakistan"<<endl;
+	cout<<"pakistan"<<endl;
+	cout<<"pakistan"<<endl;
+	cout<<"pakistan"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"c++"<<endl;
+	cout<<"1,2,3,4,5"<<endl;
+	cout<<"x,y,z"<<endl;
+	return 0;
+	}
+
+
+
+
+
+
