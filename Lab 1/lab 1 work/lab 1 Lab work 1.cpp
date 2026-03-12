0bfc5584d1737d467e106038283c1b9fcfbcb952
Abeeha <abeeha7khan@gmail.com>
2026-03-12 20:46:07 +0500

    Add files via upload

---
+++ Lab 1/lab 1 work/lab 1 Lab work 1.cpp
@@ -0,0 +1,18 @@
+
+// Name: Abeeha Khan
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
