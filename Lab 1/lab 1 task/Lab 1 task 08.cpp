a63763f3b9f64e58219b201d29a791436215d752
Abeeha <abeeha7khan@gmail.com>
2026-03-12 20:42:42 +0500

    Add files via upload

---
+++ Lab 1/lab 1 task/Lab 1 task 08.cpp
@@ -0,0 +1,17 @@
+
+// Name: Abeeha Khan
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float aB = 10.2;  // Declaring float variable
+
+    cout << aB;  // Printing value of aB
+
+    return 0;  // Program ends successfully
+}
