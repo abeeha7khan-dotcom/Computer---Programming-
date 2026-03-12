ef794f1f8dd123ae1ca5dfdbe7a3679a77e8776a
Abeeha <abeeha7khan@gmail.com>
2026-03-12 19:58:10 +0500

    Add files via upload

---
+++ Lab 1/lab 1 task/Lab 1 task 02.cpp
@@ -0,0 +1,20 @@
+
+// Name: Abeeha Khan
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>   // For input and output
+using namespace std;  // Using standard namespace
+
+int main()
+{
+    // Printing first line
+    cout << "I am in ELECTRICAL DEPARTEMENT. \n";
+
+    // Printing second line
+    cout << "I am programming enthusiast!";
+
+    return 0;  // Program ends here
+}
+
