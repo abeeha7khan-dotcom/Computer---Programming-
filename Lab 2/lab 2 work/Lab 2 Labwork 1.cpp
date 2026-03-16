63e31216a48ca1ff6acf2d17aaf0bcc9f70957cb
Abeeha <abeeha7khan@gmail.com>
2026-03-16 19:54:49 +0500

    Add files via upload

---
+++ Lab 2/lab 2 work/Lab 2 Labwork 1.cpp
@@ -0,0 +1,16 @@
+
+// Name: Abeeha khan
+// Department: Electrical Engineering (communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20; float b = 5.9; char c = 'A';
+    cout << "a=" << a << ", size=" << sizeof(a) << endl;
+    cout << "b=" << b << ", size=" << sizeof(b) << endl;
+    cout << "c=" << c << ", size=" << sizeof(c) << endl;
+    return 0;
+}
