6f06e48e5460701c751f66d255570a5ebfca8756
Abeeha <abeeha7khan@gmail.com>
2026-03-16 20:04:53 +0500

    Add files via upload

---
+++ Lab 3/lab 3 tasks/Lab 3 Drill task 2.cpp
@@ -0,0 +1,26 @@
+
+// Name: Abeeha khan
+// Department: Electrical Engineering (communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float radius = 0, circumference = 1;  // initialize radius and circumference
+
+    cout << "Enter radius of the circle: ";
+    cin >> radius;  // take input from user
+
+    circumference = 2 * 3.14159 * radius;  // calculate circumference
+
+    // display radius and circumference
+    cout << "Radius = " << radius << endl;
+    cout << "Circumference = " << circumference << endl;
+
+    return 0;  // program ends
+}
+
+
