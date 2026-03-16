b8cb6e76009970c9a536f1d2b541713fae97c0ca
Abeeha <abeeha7khan@gmail.com>
2026-03-16 19:55:44 +0500

    Add files via upload

---
+++ Lab 2/lab 2 work/Lab 2 labwork 3.cpp
@@ -0,0 +1,23 @@
+
+// Name: Abeeha khan
+// Department: Electrical Engineering (communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;  // declare variables
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;  // take input from user
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
