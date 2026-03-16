ef61068e5afb206e52bc76305235829ffc40a2f2
Abeeha <abeeha7khan@gmail.com>
2026-03-16 19:55:20 +0500

    Add files via upload

---
+++ Lab 2/lab 2 work/Lab 2 Labwork 2.cpp
@@ -0,0 +1,16 @@
+
+// Name: Abeeha khan
+// Department: Electrical Engineering (communication)
+// Registration No: BF25NWELE0663
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14;  // declare float variable pi
+    cout << "Value of pi = " << pi << endl;  // display the value
+    return 0;
+}
+
+
