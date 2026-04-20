4cb84561f5071557b55ae769fc79fbfc03908090
Abeeha <abeeha7khan@gmail.com>
2026-04-20 21:36:19 +0500

    Add files via upload

---
+++ Lab 6/lab 6 tasks/lab 6 task 02.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 0;
+    cout<<"enter your integer"<<endl;
+    cin>>num;
+   	cout<<"your table is"<<endl;
+    
+    for(int i=1; i<=1; i++)
+    {
+	
+		for(int j=1; j<=10; j++){
+	
+	cout<<num<<"*"<<j<<"= \t"<<num*j<<endl;
+}
+}
+	return 0;
+	}
